420d0256bd493bc5ae98cb0745b4bc5df22a1e5f
Victor Izuchukwu Ihemadu <bigDev@Victors-MacBook-Air.local>
2023-04-06 23:17:06 +0100

    boiler plate completed

--- focusbuddy/src/Timer.js
+++ focusbuddy/src/Timer.js
@@ -1,13 +1,19 @@
 import { CircularProgressbar, BuildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import PlayButton from "./PlayButton";
+import PauseButton from "./PauseButton";
+import SettingsButton from "./SettingsButton";
 
 function Timer() {
   return (
     <div>
-      <CircularProgressbar value={50} text={`50%`} />
-      <div style={{marginTop: '20px'}}>
+      <CircularProgressbar value={55} text={`55%`} />
+      <div style={{ marginTop: "20px" }}>
         <PlayButton />
+        <PauseButton />
+      </div>
+      <div style={{marginTop:'20px'}}>
+        <SettingsButton />
       </div>
     </div>
   );
